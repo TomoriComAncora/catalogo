bec789825b2f192b1149c4715ff04a60c2d71a46
TomoriComAncora <lucas_fernando@ufms.br>
2023-10-16 15:22:19 -0400

    FEAT: fim sessao iphone 11

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,24 @@
-console.log("Teste");
+const botoes11 = document.querySelectorAll("#escolha-iphone11 li");
+const imagem = document.querySelector("#imagem-produto");
+
+botoes11.forEach((btns) => {
+  btns.addEventListener("click", (e) => {
+    botoes11.forEach((btns) => {
+      btns.querySelector(".cor").classList.remove("selecionado");
+    });
+
+    const botao11 = e.target;
+    const id11 = botao11.getAttribute("id");
+
+    console.log(id11);
+
+    botao11.querySelector(".cor").classList.add("selecionado");
+
+    imagem.classList.add("transparencia");
+    imagem.setAttribute("src", `img/img_iphone11/iphone11_${id11}.png`);
+
+    setTimeout(() => {
+      imagem.classList.remove("transparencia");
+    }, 300);
+  });
+});
