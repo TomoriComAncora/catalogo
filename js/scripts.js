17f680ff6b109af9277fc8d8604fb96c0e3058ec
TomoriComAncora <lucas_fernando@ufms.br>
2023-10-16 16:19:23 -0400

    FEAT: iphone 12

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,9 @@
 const botoes11 = document.querySelectorAll("#escolha-iphone11 li");
 const imagem = document.querySelector("#imagem-produto");
 
+const botoes12 = document.querySelectorAll("#escolha-iphone12 li");
+const imagemIp12 = document.querySelector("#imagem-produto-12");
+
 botoes11.forEach((btns) => {
   btns.addEventListener("click", (e) => {
     botoes11.forEach((btns) => {
@@ -16,6 +19,26 @@ botoes11.forEach((btns) => {
 
     imagem.classList.add("transparencia");
     imagem.setAttribute("src", `img/img_iphone11/iphone11_${id11}.png`);
+
+    setTimeout(() => {
+      imagem.classList.remove("transparencia");
+    }, 300);
+  });
+});
+
+botoes12.forEach((btns) => {
+  btns.addEventListener("click", (e) => {
+    botoes12.forEach((btns) => {
+      btns.querySelector(".cor").classList.remove("selecionado");
+    });
+
+    const botao12 = e.target;
+    const id12 = e.target.getAttribute("id");
+
+    botao12.querySelector(".cor").classList.add("selecionado");
+
+    imagemIp12.classList.add("trasparencia");
+    imagemIp12.setAttribute("src", `img/img_iphone12/iphone12_${id12}.png`);
 
     setTimeout(() => {
       imagem.classList.remove("transparencia");
